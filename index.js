e76511be42b05f9d52c902c98b7d7d63e05c46d2
Eduardo <eduardobecman@gmail.com>
2026-05-05 19:30:24 -0300

    feat: aplicação Node.js com 3 rotas

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+            <body>
+                <h1>Página Inicial</h1>
+                <p>Bem-vindo à minha aplicação Node.js!</p>
+            </body>
+        </html>
+    `);
+});
+
+app.get('/sobre', (req, res) => {
+    res.send(`
+        <html>
+            <body>
+                <h1>Sobre</h1>
+                <p>Esta aplicação foi criada para a atividade de Node.js.</p>
+            </body>
+        </html>
+    `);
+});
+
+app.get('/contato', (req, res) => {
+    res.send(`
+        <html>
+            <body>
+                <h1>Contato</h1>
+                <p>Email: aluno@email.com</p>
+            </body>
+        </html>
+    `);
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
